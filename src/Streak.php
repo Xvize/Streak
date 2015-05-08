4ea9bb4f523127ef871ef348f776acd579ed8016
Frank Keulen <frank@keulen-musson.nl>
2015-05-08 14:25:17 +0200

    Initial commit

--- src/Streak.php
+++ src/Streak.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ *
+ * @package     Streak
+ * @version     1.0.1
+ * @author      Frank Keulen
+ * @license     MIT License
+ * @copyright   2015 Frank Keulen
+ * @link        http://github.com/xvize/streak
+ */
+
 namespace Xvize\Streak;
 
 use Xvize\Streak\StreakFunctions;
